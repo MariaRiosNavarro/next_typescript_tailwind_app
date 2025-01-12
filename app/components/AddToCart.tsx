7b65a704f351c2be16ffec861ac02eec8fee2047
Maria Rios Navarro <propscode254@gmail.com>
2025-01-12 14:30:54 +0100

    Add tailwind styles

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const AddToCart = () => {
   return (
-    <button onClick={()=>console.log('Click')}
+    <button className='btn btn-primary' onClick={()=>console.log('Click')}
     >Add to Cart</button>
   )
 }

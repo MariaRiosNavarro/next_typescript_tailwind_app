b5171343f9fa9eb665f5f4cd7fc71f4c1dc0359e
Maria Rios Navarro <propscode254@gmail.com>
2025-01-12 13:27:55 +0100

    add first components

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
 
-const UsersPage = () => {
-  return (
-    <div>UsersPage</div>
+
+interface User {
+    id: number;
+    name: string;
+}
+
+const UsersPage = async() => {
+
+    const res = await fetch("https://jsonplaceholder.typicode.com/users",
+        { cache: 'no-store'} // Daten wechseln häufig. Dynamic Reendering (npm run build -> npm start) statt Static (wichtig für wechselden Komponenten wie new Date()
+        // { next: {revalidate: 10} } //Refresh data jeder 10 sekunden, nur in "fetch" nicht in Axios
+    )
+    const users: User[] = await res.json()
+
+    return (
+        <>
+            <h1>Users</h1>
+            <p>{new Date().toLocaleTimeString()}</p>
+            <ul>
+                {users.map(user=> <li key={user.id}>{user.name}</li>)}
+            </ul>
+        </>
   )
 }
 
